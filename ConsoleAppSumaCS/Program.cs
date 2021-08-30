e9c65ff63ade55ba677448dc18920527ee2687bd
Armando <Armando@DESKTOP-4B44KC4>
2021-08-29 20:44:12 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleAppSumaCS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAppSumaCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Sumar
+            int a = 12;
+            int b = 11;
+            int c = a + b;
+
+            Console.WriteLine(c = a + b);
+            Console.ReadKey();
+        }
+    }
+}
